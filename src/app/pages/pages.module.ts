8c47671648f5591af569f3f0480ff14b1484bd79
Alfonso José Ferrando <alfonsjfp@gmail.com>
2021-07-08 22:50:29 +0200

    Fin sección 6- Componentes reutilizables

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ComponentsModule } from '../components/components.module';
+
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -19,8 +23,10 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [ 
     CommonModule,
+    FormsModule,
     SharedModule,
-    RouterModule
+    RouterModule,
+    ComponentsModule
  ],
   exports: [
     DashboardComponent,
